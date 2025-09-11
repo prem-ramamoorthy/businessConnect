897d735fdd69e8551e254b7ecd6d7cf6d80fd2fb
prem-ramamoorthy <pkrprem2005@gmail.com>
2025-09-11 22:43:34 +0530

    small changes

--- src/MainPage/Objectives/Objective.jsx
+++ src/MainPage/Objectives/Objective.jsx
@@ -1,104 +1,106 @@
 import './ObjectiveStyle.css'
 
 function Objective() {
-  return (
-    <section className="objectives-section">
-        <h2 className="section-title">Our Objectives</h2>
+    return (
+        <div className='objective'>
+            <section className="objectives-section">
+                <h2 className="section-title">Our Objectives</h2>
 
-        <div className="objectives-container">
-            <div className="objectives-image">
-                <img src="/src/assets/img2.jpg" alt="Community Objectives Illustration" />
-            </div>
+                <div className="objectives-container">
+                    <div className="objectives-image">
+                        <img src="/src/assets/img2.jpg" alt="Community Objectives Illustration" />
+                    </div>
 
-            <div className="objectives-grid">
-                <div className="objective-card">
-                    <div className="objective-keyword">
-                        <h4>Entrepreneurship</h4>
-                        <span>தொழில்முனைவு</span>
-                    </div>
-                    <div className="objective-details">
-                        <p>செங்குந்த சமுதாய மக்களை தொழில் முனைவோர்களாக ஊக்குவித்து தொழில் தொடங்க வழிகாட்டுதல்.</p>
+                    <div className="objectives-grid">
+                        <div className="objective-card">
+                            <div className="objective-keyword">
+                                <h4>Entrepreneurship</h4>
+                                <span>தொழில்முனைவு</span>
+                            </div>
+                            <div className="objective-details">
+                                <p>செங்குந்த சமுதாய மக்களை தொழில் முனைவோர்களாக ஊக்குவித்து தொழில் தொடங்க வழிகாட்டுதல்.</p>
+                            </div>
+                        </div>
+                        <div className="objective-card">
+                            <div className="objective-keyword">
+                                <h4>Networking</h4>
+                                <span>Connections</span>
+                            </div>
+                            <div className="objective-details">
+                                <p>ஒன்றிணைத்து Networking வணிக தொடர்புகளை விரிவாக்குதல்.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="objective-card">
+                            <div className="objective-keyword">
+                                <h4>Training</h4>
+                                <span>பயிற்சி</span>
+                            </div>
+                            <div className="objective-details">
+                                <p>தொழில் முனைவோர்களுக்கு பயிற்சி, Start-up, Leadership
+                                    போன்றவற்றை வழிகாட்டுதல்.</p>
+                            </div>
+                        </div>
+                        <div className="objective-card">
+                            <div className="objective-keyword">
+                                <h4>Weaving</h4>
+                                <span>நெசவு</span>
+                            </div>
+                            <div className="objective-details">
+                                <p>நமது குலத்தொழில் நெசவு பற்றிய பயிலரங்கம், பயிற்சிகள் வழங்குவது.</p>
+                            </div>
+                        </div>
+                        <div className="objective-card">
+                            <div className="objective-keyword">
+                                <h4>Women Empowerment</h4>
+                                <span>பெண்கள் முன்னேற்றம்</span>
+                            </div>
+                            <div className="objective-details">
+                                <p>நமது சமுதாய பெண்களின் தொழில் முன்னேற்றத்திற்கு உதவுவது.</p>
+                            </div>
+                        </div>
+                        <div className="objective-card">
+                            <div className="objective-keyword">
+                                <h4>Youth Development</h4>
+                                <span>இளைஞர் மேம்பாடு</span>
+                            </div>
+                            <div className="objective-details">
+                                <p>நமது சமுதாய இளைஞர்களை தொழில் முனைவோர்களாக உருவாக்குவது.</p>
+                            </div>
+                        </div>
+                        <div className="objective-card">
+                            <div className="objective-keyword">
+                                <h4>Leadership</h4>
+                                <span>தலைமைத்துவம்</span>
+                            </div>
+                            <div className="objective-details">
+                                <p>பொறுப்புணர்ச்சி, தன்னம்பிக்கை, மற்றும் தலைமைப் பண்பு பயிற்சிகளை
+                                    வழங்குவது.</p>
+                            </div>
+                        </div>
+                        <div className="objective-card">
+                            <div className="objective-keyword">
+                                <h4>Heritage Growth</h4>
+                                <span>பாரம்பரிய வளர்ச்சி</span>
+                            </div>
+                            <div className="objective-details">
+                                <p>பாரம்பரிய Textile, Weaving போன்றவற்றை நவீனமாக்கி உலகளவில் கொண்டு செல்வது.</p>
+                            </div>
+                        </div>
+                        <div className="objective-card">
+                            <div className="objective-keyword">
+                                <h4>Student Awareness</h4>
+                                <span>மாணவர் விழிப்புணர்வு</span>
+                            </div>
+                            <div className="objective-details">
+                                <p>கல்லூரி மாணவர்கள் படிக்கும் போதே அவர்களுக்கு தொழில் பற்றிய விழிப்புணர்வு ஏற்படுத்துதல்.</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="objective-card">
-                    <div className="objective-keyword">
-                        <h4>Networking</h4>
-                        <span>Connections</span>
-                    </div>
-                    <div className="objective-details">
-                        <p>ஒன்றிணைத்து Networking வணிக தொடர்புகளை விரிவாக்குதல்.
-                        </p>
-                    </div>
-                </div>
-                <div className="objective-card">
-                    <div className="objective-keyword">
-                        <h4>Training</h4>
-                        <span>பயிற்சி</span>
-                    </div>
-                    <div className="objective-details">
-                        <p>தொழில் முனைவோர்களுக்கு பயிற்சி, Start-up, Leadership
-                            போன்றவற்றை வழிகாட்டுதல்.</p>
-                    </div>
-                </div>
-                <div className="objective-card">
-                    <div className="objective-keyword">
-                        <h4>Weaving</h4>
-                        <span>நெசவு</span>
-                    </div>
-                    <div className="objective-details">
-                        <p>நமது குலத்தொழில் நெசவு பற்றிய பயிலரங்கம், பயிற்சிகள் வழங்குவது.</p>
-                    </div>
-                </div>
-                <div className="objective-card">
-                    <div className="objective-keyword">
-                        <h4>Women Empowerment</h4>
-                        <span>பெண்கள் முன்னேற்றம்</span>
-                    </div>
-                    <div className="objective-details">
-                        <p>நமது சமுதாய பெண்களின் தொழில் முன்னேற்றத்திற்கு உதவுவது.</p>
-                    </div>
-                </div>
-                <div className="objective-card">
-                    <div className="objective-keyword">
-                        <h4>Youth Development</h4>
-                        <span>இளைஞர் மேம்பாடு</span>
-                    </div>
-                    <div className="objective-details">
-                        <p>நமது சமுதாய இளைஞர்களை தொழில் முனைவோர்களாக உருவாக்குவது.</p>
-                    </div>
-                </div>
-                <div className="objective-card">
-                    <div className="objective-keyword">
-                        <h4>Leadership</h4>
-                        <span>தலைமைத்துவம்</span>
-                    </div>
-                    <div className="objective-details">
-                        <p>பொறுப்புணர்ச்சி, தன்னம்பிக்கை, மற்றும் தலைமைப் பண்பு பயிற்சிகளை
-                            வழங்குவது.</p>
-                    </div>
-                </div>
-                <div className="objective-card">
-                    <div className="objective-keyword">
-                        <h4>Heritage Growth</h4>
-                        <span>பாரம்பரிய வளர்ச்சி</span>
-                    </div>
-                    <div className="objective-details">
-                        <p>பாரம்பரிய Textile, Weaving போன்றவற்றை நவீனமாக்கி உலகளவில் கொண்டு செல்வது.</p>
-                    </div>
-                </div>
-                <div className="objective-card">
-                    <div className="objective-keyword">
-                        <h4>Student Awareness</h4>
-                        <span>மாணவர் விழிப்புணர்வு</span>
-                    </div>
-                    <div className="objective-details">
-                        <p>கல்லூரி மாணவர்கள் படிக்கும் போதே அவர்களுக்கு தொழில் பற்றிய விழிப்புணர்வு ஏற்படுத்துதல்.</p>
-                    </div>
-                </div>
-            </div>
+            </section>
         </div>
-    </section>
-  )
+    )
 }
 
 export default Objective
